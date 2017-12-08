b5b2fa29b548ffa9abb0c00f5b6ce0b2bfcab081
Zirland <git@zirland.org>
2017-12-08 19:40:14 +0100

    Migrace

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <body>
 
 <?php
-$link = mysqli_connect('localhost', 'root', 'root', 'JDF');
+$link = mysqli_connect('localhost', 'gtfs', 'gtfs', 'JDF');
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
