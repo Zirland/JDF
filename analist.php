e0b082b4961a833cf94106ca2dc1e409be9eee93
Zirland <git@zirland.org>
2017-12-24 15:11:53 +0100

    List analysis

--- analist.php
+++ analist.php
@@ -8,7 +8,7 @@ $dnes_mesic = date("n", time());
 $dnes_rok = date("Y", time());
 $today = mktime(0,0,0,$dnes_mesic,$dnes_den,$dnes_rok);
 
-$query6 = "SELECT DISTINCT route_id, route_name, route_type FROM analyza WHERE route_id IN (SELECT DISTINCT route_id FROM anal_done) ORDER BY route_id;"; // WHERE route_id LIKE '5950%' 
+$query6 = "SELECT DISTINCT route_id, route_name, route_type FROM analyza WHERE route_id IN (SELECT DISTINCT route_id FROM anal_done) ORDER BY route_id;";
 if ($result6 = mysqli_query($link, $query6)) {
 	while ($row6 = mysqli_fetch_row($result6)) {
 		$route_id = $row6[0];
@@ -20,25 +20,27 @@ if ($result6 = mysqli_query($link, $query6)) {
 		$halt = 0;
 		$label = "";
 		
-		$query11 = "SELECT DISTINCT analyza.dir, analyza.verze, analyza.datumod, analyza.datumdo, anal_done.datumod FROM analyza LEFT JOIN anal_done ON analyza.route_id = anal_done.route_id WHERE analyza.route_id = '$route_id' ORDER BY analyza.datumod DESC;";
+		$query11 = "SELECT DISTINCT dir, verze, datumod, datumdo FROM analyza WHERE route_id = '$route_id' ORDER BY datumod DESC;";
 		if ($result11 = mysqli_query($link, $query11)) {
 			while ($row11 = mysqli_fetch_row($result11)) {
 				$dir = $row11[0];
 				$verze = $row11[1];
 				$datumod = $row11[2];
 				$datumdo = $row11[3];
-				$platnost = $row11[4];
+
+				$query31 = mysqli_query($link, "SELECT * FROM anal_done WHERE route_id = '$route_id' AND datumod = '$datumod';");
+				$platnost = mysqli_num_rows($query31);
 
 				$od_den = substr($datumod,-2);
 				$od_mesic = substr($datumod,5,2);
 				$od_rok = substr($datumod,0,4);
 				$od_time = mktime(0,0,0,$od_mesic,$od_den,$od_rok);
 
-				if ($platnost == $datumod) {echo "<i>";}
+				if ($platnost > 0) {echo "<i>";}
 				if ($od_time > $today) {$label = "F";}
 				if ($od_time <= $today) {$label = "";}
 				if ($halt == 0) {echo "$dir ($verze) $datumod > $datumdo $label > <a href=\"genroute.php?file=$dir&mode=$label&linkaod=$datumod\" target=\"_blank\">Generovat</a><br />";}
-				if ($platnost == $datumod) {echo "</i>";}
+				if ($platnost > 0) {echo "</i>";}
 				if ($od_time <= $today) {$halt = 1;}
 			}
 		}
@@ -48,7 +50,7 @@ if ($result6 = mysqli_query($link, $query6)) {
 
 echo "<hr>";
 
-$query6 = "SELECT DISTINCT route_id, route_name, route_type FROM analyza WHERE route_id NOT IN (SELECT DISTINCT route_id FROM anal_done) ORDER BY route_id;"; // WHERE route_id LIKE '5950%' 
+$query6 = "SELECT DISTINCT route_id, route_name, route_type FROM analyza WHERE route_id NOT IN (SELECT DISTINCT route_id FROM anal_done) ORDER BY route_id;";
 if ($result6 = mysqli_query($link, $query6)) {
 	while ($row6 = mysqli_fetch_row($result6)) {
 		$route_id = $row6[0];
